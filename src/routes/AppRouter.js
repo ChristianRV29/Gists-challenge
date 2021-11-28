c09016297ac424fefcdfa58b8d097f741b8ff00d
ChristianRV29 <kmylorv@gmail.com>
2021-11-28 14:23:47 -0500

    First routes and navbar set

---
+++ src/routes/AppRouter.js
@@ -0,0 +1,18 @@
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
+import { Navbar } from '../components/organisms/Navbar';
+import { LoginScreen } from '../screens/LoginScreen';
+import { NotFoundScreen } from '../screens/NotFoundScreen';
+import { PostsScreen } from '../screens/PostsScreen';
+
+export const AppRouter = () => {
+    return (
+        <BrowserRouter>
+            <Navbar />
+            <Routes>
+                <Route path="/" element={<PostsScreen />} />
+                <Route path="/login" element={<LoginScreen />} />
+                <Route path="*" element={<NotFoundScreen />} />
+            </Routes>
+        </BrowserRouter>
+    )
+}
